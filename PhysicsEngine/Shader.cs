575edee2435d65d722255d00f488bad49d37d15f
KingHarley <cweiss1995@gmail.com>
2023-04-12 22:45:43 +0800

    Adding shaders and modifying window

---
+++ PhysicsEngine/Shader.cs
@@ -0,0 +1,28 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhysicsEngine
+{
+    internal class Shader
+    {
+        int Handle;
+        int VertexHandle;
+        int FragmentHandle;
+        public Shader(string vertexShaderFile, string fragmentShaderFile)
+        {
+            VertexHandle = GL.CreateShader(ShaderType.VertexShader);
+            var vertexSource = File.ReadAllText(vertexShaderFile);
+            GL.ShaderSource(VertexHandle, vertexSource);
+
+            FragmentHandle = GL.CreateShader(ShaderType.FragmentShader);
+            var fragmentSource = File.ReadAllText(fragmentShaderFile);
+            GL.ShaderSource(FragmentHandle, fragmentSource);
+
+            GL.CompileShader(VertexHandle);
+        }
+    }
+}
--- PhysicsEngine/Window.cs
+++ PhysicsEngine/Window.cs
@@ -1,4 +1,7 @@
-﻿using OpenTK.Windowing.Desktop;
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +12,39 @@ namespace PhysicsEngine
 {
     internal class Window : GameWindow
     {
+        private float[] Square = { -0.5f, -0.5f, -0.5f, -0.5f, 0.5f, 0.5f, 0.5f, 0.5f, -0.5f };
+        private int VertexBufferObject;
         public Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings)
         {
 
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+            if(IsKeyPressed(OpenTK.Windowing.GraphicsLibraryFramework.Keys.Escape))
+                Close();
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+            GL.ClearColor(Color4.Black);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, Square.Length * sizeof(float), Square, BufferUsageHint.StaticDraw);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            SwapBuffers();
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+            GL.Viewport(0, 0, e.Width, e.Height);
         }
     }
 }
